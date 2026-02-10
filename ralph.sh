5e569548c4faeb17e1bae33d088e477ad4fa60f6
wuhaa <361230+wuhaa@users.noreply.github.com>
2026-02-09 21:19:31 -0500

    Update ralph.sh

--- ralph.sh
+++ ralph.sh
@@ -14,8 +14,8 @@ while [ $COUNT -lt $MAX ]; do
         echo -e "     -- Running iteration ${COUNT} --     "
         echo -e "\n\n\n\n\n\n"
         PROMPT=$(cat $1)
-        kiro-cli chat --no-interactive --trust-all-tools "Continue working on the described task: $PROMPT. Update context.md with your progress and next steps before exiting."
-        # claude --allowedTools "Read,Edit,Bash" -p "Continue working on the described task: $PROMPT. Update context.md with your progress and next steps before exiting."
+        kiro-cli chat --no-interactive --trust-all-tools "Continue working on this task: $PROMPT. Update context.md with your progress and next steps before exiting."
+        # claude --allowedTools "Read,Edit,Bash" -p "Continue working on this task: $PROMPT. Update context.md with your progress and next steps before exiting."
         echo -e "\n\n\n\n\n\n"
         echo -e "     -- Pausing for 5 sec --     "
         echo -e "\n\n\n\n\n\n"
